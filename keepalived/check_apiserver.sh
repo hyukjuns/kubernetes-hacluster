0f625082057daf9b93b46c08bedbf481bd44a766
namhj94 <hyukjun1994@gmail.com>
2020-09-05 18:35:32 +0900

    update

---
+++ keepalived/check_apiserver.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+errorExit() {
+    echo "*** $*" 1>&2
+    exit 1
+}
+export APISERVER_VIP=192.168.122.10
+export APISERVER_DEST_PORT=6443
+
+
+curl --silent --max-time 2 --insecure https://localhost:${APISERVER_DEST_PORT}/ -o /dev/null || errorExit "Error GET https://localhost:${APISERVER_DEST_PORT}/"
+if ip addr | grep -q ${APISERVER_VIP}; then
+    curl --silent --max-time 2 --insecure https://${APISERVER_VIP}:${APISERVER_DEST_PORT}/ -o /dev/null || errorExit "Error GET https://${APISERVER_VIP}:${APISERVER_DEST_PORT}/"
+fi
